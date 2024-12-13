a8af4b8e7ee43a2052060aa5858e26031aee9a30
Mobina <mobina.yousefian203@gmail.com>
2024-12-13 05:18:37 +0330

    [ADD] dynamic route example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-export default function Home() {
+type Post = {
+    id: number,
+    title: string,
+}
+
+export default async function Home() {
+    const res: Response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store"
+    });
+
+    const posts: Post[] = await res.json();
+
     return (
-        <h1>به تماشاچی خوش اومدی!</h1>
+        <main>
+            <h1>به تماشاچی خوش اومدی!</h1>
+            <hr/>
+            <h2>Test Request (Dynamic Route)</h2>
+            <ul>
+                {
+                    posts.map((post: Post) => (
+                        <li key={post.id}>{post.title}</li>
+                    ))
+                }
+            </ul>
+        </main>
     );
 }
