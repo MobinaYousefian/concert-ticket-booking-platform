e7a1a0bccff604d506b11a39db6c524c1fd0cac3
Mobina <mobina.yousefian203@gmail.com>
2024-12-21 00:12:15 +0330

    [CHORE] apply prettier changes on all files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Vazirmatn } from 'next/font/google'
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
 const vazirmatn = Vazirmatn({
-    subsets: ['latin', 'arabic'],
-    display: 'swap',
-})
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "تماشاچی | خرید آنلاین بلیط کنسرت و تئاتر",
@@ -20,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir={"rtl"} className={vazirmatn.className}>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
 type Post = {
-    id: number,
-    title: string,
-}
+  id: number;
+  title: string;
+};
 
 export default async function Home() {
-    const res: Response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        cache: "no-store"
-    });
+  const res: Response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts",
+    {
+      cache: "no-store",
+    },
+  );
 
-    const posts: Post[] = await res.json();
+  const posts: Post[] = await res.json();
 
-    return (
-        <main>
-            <h1>به تماشاچی خوش اومدی!</h1>
-            <hr/>
-            <h2>Test Request (Dynamic Route)</h2>
-            <ul>
-                {
-                    posts.map((post: Post) => (
-                        <li key={post.id}>{post.title}</li>
-                    ))
-                }
-            </ul>
-        </main>
-    );
+  return (
+    <main>
+      <h1>به تماشاچی خوش اومدی!</h1>
+      <hr />
+      <h2>Test Request (Dynamic Route)</h2>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
