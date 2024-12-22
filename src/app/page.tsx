bfadaeb6ecd57cdbe9b9f7eb2724953adcb99c20
Mobina <mobina.yousefian203@gmail.com>
2024-12-23 02:55:48 +0330

    [UPDATE] home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default async function Home() {
-  return <></>;
+import { ReactElement } from "react";
+
+import GlobalSearchBoxComponent from "@/components/global-search-box/global-search-box.component";
+import TamashachiLogo from "@/logo/tamashachi-logo";
+
+import styles from "./page.module.css";
+
+export default function Home(): ReactElement {
+  return (
+    <div className={styles.home}>
+      <h1>
+        <TamashachiLogo />
+        تماشاچی
+      </h1>
+      <GlobalSearchBoxComponent />
+      <div className={styles.history}>
+        <div className={styles.title}>آخرین جستجوهای شما</div>
+        <ul>
+          <li>کنسرت تهران</li>
+          <li>نمایش پرویز و پریزاد</li>
+          <li>محسن یگانه</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
