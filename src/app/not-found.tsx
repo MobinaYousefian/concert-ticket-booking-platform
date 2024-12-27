bc5bf28b3d405a75452aed4db62011178195367f
Mobina <mobina.yousefian203@gmail.com>
2024-12-27 04:00:27 +0330

    [ADD] not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import quietStreet from "@/assets/illustrations/quiet-street.svg";
+
+import styles from "./not-found.module.css";
+
+export default function NotFound(): ReactElement {
+  return (
+    <div className={styles["not-found"]}>
+      <div className={styles.writings}>
+        <h1>۴۰۴</h1>
+        <h2>صفحه مورد نظر یافت نشد!</h2>
+      </div>
+      <div className={styles.cta}>
+        <Link href={"/"}>
+          <button>بازگشت به خانه</button>
+        </Link>
+      </div>
+      <div className={styles.visuals}>
+        <Image src={quietStreet} alt={""} width={272} height={272} priority />
+      </div>
+    </div>
+  );
+}
