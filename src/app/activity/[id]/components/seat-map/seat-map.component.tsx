0c71129149da49a86acc5fef098b619d3d80854a
Mobina <mobina.yousefian203@gmail.com>
2025-02-19 01:45:48 +0330

    [FIX] responsive canvas width and height

--- src/app/activity/[id]/components/seat-map/seat-map.component.tsx
+++ src/app/activity/[id]/components/seat-map/seat-map.component.tsx
@@ -1,22 +1,36 @@
 "use client";
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import dynamic from "next/dynamic";
 
 import styles from "./seat-map.module.css";
 
-export default function SeatMapComponent(): ReactElement {
-  const canvasWidth = 1000;
-  const canvasHeight = 1000;
+const Canvas = dynamic(
+  () => import("@/app/activity/[id]/components/canvas/canvas.component"),
+  {
+    ssr: false,
+  },
+);
 
-  const Canvas = dynamic(
-    () => import("@/app/activity/[id]/components/canvas/canvas.component"),
-    {
-      ssr: false,
-    },
-  );
+export default function SeatMapComponent(): ReactElement {
+  const sectionRef = useRef(null);
+  const [canvasWidth, setCanvasWidth] = useState(0);
+  const [canvasHeight, setCanvasHeight] = useState(0);
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((event) => {
+      setCanvasWidth(event[0].contentBoxSize[0].inlineSize);
+      setCanvasHeight(event[0].contentBoxSize[0].blockSize);
+    });
+
+    if (sectionRef.current) {
+      resizeObserver.observe(sectionRef.current);
+    }
+
+    return () => resizeObserver.disconnect();
+  }, [sectionRef]);
 
   return (
-    <section className={styles["seat-map"]}>
+    <section className={styles["seat-map"]} ref={sectionRef}>
       <Canvas width={canvasWidth} height={canvasHeight} />
     </section>
   );
