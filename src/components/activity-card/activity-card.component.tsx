b189867dd3379552fac3b717c1d9e018c83c0aef
Mobina <mobina.yousefian203@gmail.com>
2025-03-06 01:27:17 +0330

    [ADD] sold out label

--- src/components/activity-card/activity-card.component.tsx
+++ src/components/activity-card/activity-card.component.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { clsx } from "clsx";
+
 import ButtonComponent from "@/components/button/button.component";
 
 import HugeiconsCalendar03 from "@/icons/HugeiconsCalendar03";
@@ -9,6 +11,8 @@ import HugeiconsLocation04 from "@/icons/HugeiconsLocation04";
 import HugeiconsShoppingBag02 from "@/icons/HugeiconsShoppingBag02";
 
 import { Activity } from "@/lib/data.type";
+
+import soldOutImage from "@/assets/images/sold-out-label.png";
 
 import styles from "./activity-card.module.css";
 
@@ -21,18 +25,25 @@ export default function ActivityCardComponent({
 }: Props): ReactElement {
   const ACTIVITY_PAGE_URL = `/activity/${activity.id.toString()}`;
 
+  const isSoldOut = activity.remainingShowings < 1;
+
   return (
     <section className={styles["activity-card"]}>
       <div className={styles.visuals}>
-        {/* todo: add sold out image here */}
         <Image
           src={activity.banner}
           alt={activity.title}
           width={500}
           height={500}
+          className={clsx(isSoldOut && styles.dark)}
         />
-        <div className={styles.badge}>{activity.city}</div>
         <div className={styles.overlay}></div>
+        {isSoldOut && (
+          <div className={styles["sold-out"]}>
+            <Image src={soldOutImage} alt={""} />
+          </div>
+        )}
+        <div className={styles.badge}>{activity.city}</div>
       </div>
       <div className={styles.writings}>
         <h3 className={styles.title}>{activity.title}</h3>
@@ -47,9 +58,15 @@ export default function ActivityCardComponent({
       </div>
       <div className={styles.cta}>
         <Link href={ACTIVITY_PAGE_URL}>
-          <ButtonComponent className={styles.buy}>
-            <HugeiconsShoppingBag02 />
-            خرید بلیت
+          <ButtonComponent className={styles.buy} disabled={isSoldOut}>
+            {isSoldOut ? (
+              "بلیت‌ تمام شد"
+            ) : (
+              <>
+                <HugeiconsShoppingBag02 />
+                خرید بلیت
+              </>
+            )}
           </ButtonComponent>
         </Link>
       </div>
